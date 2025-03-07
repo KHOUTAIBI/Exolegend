8a5611b8a9f923f445401b2a23b8595271397803
KHOUTAIBI <khoutaibi.iliass@gmail.com>
2025-03-07 15:37:28 +0100

    added command line to be used to stuff

--- src/headers/SomethingRobotics.h
+++ src/headers/SomethingRobotics.h
@@ -13,5 +13,22 @@ enum STATES{
     BOMB,
 };
 
+STATES currState = STATES::MOVE;
+
+inline void command(){
+    if (currState == STATES::MOVE){
+        
+    }
+    else if (currState == STATES::ATTACK){
+        
+    }
+    else if (currState == STATES::FLEE){
+        
+    }
+    else if (currState == STATES::BOMB) {
+
+    }
+}
+
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,28 @@
 #include "gladiator.h"
+#include "./headers/SomethingRobotics.h"
+
 Gladiator *gladiator;
 
-
-
 void reset();
-void setup()
-{
-    // instanciation de l'objet gladiator
+void setup(){
     gladiator = new Gladiator();
-    // enregistrement de la fonction de reset qui s'éxecute à chaque fois avant qu'une partie commence
     gladiator->game->onReset(&reset); // GFA 4.4.1
 }
 
-void reset()
-{
-    // fonction de reset:
-    // initialisation de toutes vos variables avant le début d'un match
+void reset(){
     gladiator->log("Call of reset function"); // GFA 4.5.1
 }
 
-void loop()
-{
-    if (gladiator->game->isStarted())
-    { // tester si un match à déjà commencer
-        // code de votre stratégie
-        gladiator->log("Hello world - Game Started"); // GFA 4.5.1
+void loop(){
+    if (gladiator->game->isStarted()){
+        
+        gladiator->log("Game has begun"); // GFA 4.5.1
         gladiator->control->setWheelSpeed(WheelAxis::LEFT, 0.2); 
         gladiator->control->setWheelSpeed(WheelAxis::RIGHT, 0.2); 
     }
-    else
-    {
-        gladiator->log("Hello world - Game not Startd yet"); // GFA 4.5.1
+    else{
+        gladiator->log("Game has not Startd yet"); // GFA 4.5.1
     }
+
     delay(300);
 }
