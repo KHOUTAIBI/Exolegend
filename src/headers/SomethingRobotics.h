9a1d42ba5c7d593c96ae5466a2d624ad2e72cc26
KHOUTAIBI <khoutaibi.iliass@gmail.com>
2025-03-07 15:23:32 +0100

    added src to code

---
+++ src/headers/SomethingRobotics.h
@@ -0,0 +1,17 @@
+#ifndef SOMETHINGROBOTICS_H
+#define SOMETHINGROBOTICS_H
+
+#include <algorithm>
+#include <cstdio>
+#include <iostream>
+
+// Defining the states 
+enum STATES{
+    MOVE,
+    ATTACK,
+    FLEE,
+    BOMB,
+};
+
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,36 @@
+#include "gladiator.h"
+Gladiator *gladiator;
+
+
+
+void reset();
+void setup()
+{
+    // instanciation de l'objet gladiator
+    gladiator = new Gladiator();
+    // enregistrement de la fonction de reset qui s'éxecute à chaque fois avant qu'une partie commence
+    gladiator->game->onReset(&reset); // GFA 4.4.1
+}
+
+void reset()
+{
+    // fonction de reset:
+    // initialisation de toutes vos variables avant le début d'un match
+    gladiator->log("Call of reset function"); // GFA 4.5.1
+}
+
+void loop()
+{
+    if (gladiator->game->isStarted())
+    { // tester si un match à déjà commencer
+        // code de votre stratégie
+        gladiator->log("Hello world - Game Started"); // GFA 4.5.1
+        gladiator->control->setWheelSpeed(WheelAxis::LEFT, 0.2); 
+        gladiator->control->setWheelSpeed(WheelAxis::RIGHT, 0.2); 
+    }
+    else
+    {
+        gladiator->log("Hello world - Game not Startd yet"); // GFA 4.5.1
+    }
+    delay(300);
+}
